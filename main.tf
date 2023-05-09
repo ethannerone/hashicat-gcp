4806056ca3e007d9e45fcd4ac3ef20fb681e97be
ethannerone <125397148+ethannerone@users.noreply.github.com>
2023-05-09 11:50:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,8 +50,7 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  department ="devops"
-  billable="true"
+  
 
   boot_disk {
     initialize_params {
@@ -73,6 +72,8 @@ resource "google_compute_instance" "hashicat" {
 
   labels = {
     name = "hashicat"
+    department ="devops"
+    billable="true"
   }
 
 }
